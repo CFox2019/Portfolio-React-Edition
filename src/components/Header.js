8eb9d5341db0d7959b107266d2f40c569e288fd6
Courtney Fox <courtney.fox@me.com>
2021-05-03 14:45:18 -0400

    Updating dirs

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+  return (
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-md">
+        <a class="navbar-brand" href="#">Courtney Fox</a>
+      </div>
+    </nav>
+  )
+}
+
+export default Header;
