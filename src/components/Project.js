a3baa75c13efca4776138fd8d155fce1b6490703
Courtney Fox <courtney.fox@me.com>
2021-05-07 22:56:13 -0400

    created project component

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,7 +1,19 @@
 import React from "react";
 
-function Project() {
-  
+function Project(props) {
+  const imgSrc = `./assets/${props.imgName}.png`
+  return (
+    <>
+      <h4>{props.title}</h4>
+      <div className="links">
+        <a href={props.deployedLink}>Deployed Link |</a>
+        <a href={props.githubLink}>GitHub Repository</a>
+      </div>
+      <a href={props.deployedLink}>
+        <img src={imgSrc} width="350" className="img-responsive image-thumbnail" id="weather-dashboard-img" alt="Weather Dashboard" />
+      </a>
+    </>
+  )
 }
 
 export default Project;
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Row from "../components/Row";
 import Col from "../components/Col";
+import Project from '../components/Project';
 
 function Portfolio() {
   return (
@@ -12,50 +13,38 @@ function Portfolio() {
       </Row>
       <Row>
         <Col>
-          <h4>Shop Your Pantry</h4>
-          <div className="links">
-            <div className="links">
-              <a href="https://caitie-gorham.github.io/shopYourPantry/">Deployed Link |</a>
-              <a href="https://github.com/CFox2019/shopYourPantry">GitHub Repository</a>
-            </div>
-          </div>
-          <a href="https://cfox2019.github.io/shopYourPantry/">
-            <img src="./assets/shop-your-pantry.png" width="350" className="img-responsive image-thumbnail" id="shop-your-pantry-img" alt="Shop Your Pantry" />
-          </a>
+        <Project
+            title="Shop Your Pantry"
+            deployedLink="https://caitie-gorham.github.io/shopYourPantry/"
+            githubLink="https://github.com/CFox2019/shopYourPantry"
+            imgName="shop-your-pantry"
+          />
         </Col>
         <Col>
-          <h4>Dragonfly Horoscopes</h4>
-          <div className="links">
-            <div className="links">
-              <a href="https://dragonfly-horoscopes.herokuapp.com/">Heroku Link |</a>
-              <a href="https://github.com/CFox2019/Dragonfly-Horoscopes">GitHub Repository</a>
-            </div>
-          </div>
-          <a href="https://dragonfly-horoscopes.herokuapp.com/">
-            <img src="./assets/dragonfly-horoscopes.png" width="350" className="img-responsive image-thumbnail" id="shop-your-pantry-img" alt="Shop Your Pantry" />
-          </a>
+          <Project
+            title="Dragonfly Horoscopes"
+            deployedLink="https://dragonfly-horoscopes.herokuapp.com/"
+            githubLink="https://github.com/CFox2019/Dragonfly-Horoscopes"
+            imgName="dragonfly-horoscopes"
+          />
         </Col>
       </Row>
       <Row>
         <Col>
-          <h4>Work Day Scheduler</h4>
-          <div className="links">
-            <a href="https://cfox2019.github.io/Work-Day-Scheduler/">Deployed Link |</a>
-            <a href="https://github.com/CFox2019/Work-Day-Scheduler">GitHub Repository</a>
-          </div>
-          <a href="https://cfox2019.github.io/Work-Day-Scheduler/">
-            <img src="./assets/work-day-scheduler.png" width="350" className="img-responsive image-thumbnail" id="work-day-scheduler-img" alt="Work Day Scheduler" />
-          </a>
+          <Project
+              title="Work Day Schedule"
+              deployedLink="https://cfox2019.github.io/Work-Day-Scheduler/"
+              githubLink="https://github.com/CFox2019/Work-Day-Scheduler"
+              imgName="work-day-scheduler"
+          />
         </Col>
         <Col>
-          <h4>Weather Dashboard</h4>
-          <div className="links">
-            <a href="https://cfox2019.github.io/Weather-Dashboard/">Deployed Link |</a>
-            <a href="https://github.com/CFox2019/Weather-Dashboard">GitHub Repository</a>
-          </div>
-          <a href="https://cfox2019.github.io/Weather-Dashboard/">
-            <img src="./assets/weather-dashboard.png" width="350" className="img-responsive image-thumbnail" id="weather-dashboard-img" alt="Weather Dashboard" />
-          </a>
+          <Project
+              title="Weather Dashboard"
+              deployedLink="https://cfox2019.github.io/Weather-Dashboard/"
+              githubLink="https://github.com/CFox2019/Weather-Dashboard"
+              imgName="weather-dashboard"
+          />
         </Col>
       </Row>
     </>
