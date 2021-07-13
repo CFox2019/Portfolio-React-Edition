77eaa3da86106e5e9fb4dde42ac50f2cdfe1cb73
Courtney Fox <courtney.fox@me.com>
2021-07-13 13:28:04 -0400

    Updates

--- src/components/Title.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "../components/css/Title.css";
-
-function Title(props) {
-  return <h1 className="title-h1 col-md-12">{props.pageTitle}</h1>;
-}
-
-export default Title;
