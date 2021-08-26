695cbef43f1894b0216b818e1473709bfff709f0
Courtney Fox <courtney.fox@me.com>
2021-08-26 15:20:48 -0400

    updated image screenshots and sizes and made mobile first responsive

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,17 +3,26 @@ import "../components/css/Project.css";
 
 function Project(props) {
   return (
-    <div className="project-container">
-      <h4>{props.title}</h4>
-      <div className="links">
-        <a className="project-link" href={props.deployedLink}>Deployed Link |</a>
-        <a className="project-link" href={props.githubLink}> GitHub Repository</a>
+    <div className="card-container h-100">
+      <div className="card h-100">
+        <img src={props.img} className="card-img-top" alt={props.title}></img>
+        <div className="card-body">
+          <h3 className="card-title">{props.title}</h3>
+          <div className="links">
+            <a className="project-link" href={props.deployedLink}>Deployed Link |</a>
+            <a className="project-link" href={props.githubLink}> GitHub Repository</a>
+          </div>
+          <div className="summary">
+            <p>{props.summary}</p>
+          </div>
+          <div className="technologies">
+            <p>{props.technologies}</p>
+          </div>
+        </div>
       </div>
-      <a href={props.deployedLink}>
-        <img src={props.img} className="img-responsive image-thumbnail" alt="Portfolio Project" />
-      </a>
     </div>
-  )
+      )
 }
 
-export default Project;
+      export default Project;
+
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -4,12 +4,12 @@ import Col from "../components/Col";
 import Title from '../components/Title';
 import Project from '../components/Project';
 import "../components/css/Project.css";
-import fitConnect from "../assets/fit-connect.png";
-import shopYourPantry from "../assets/shop-your-pantry.png";
-import dragonflyHoroscopes from "../assets/dragonfly-horoscopes.png";
-import workDayScheduler from "../assets/work-day-scheduler.png";
-import weatherDashboard from "../assets/weather-dashboard.png";
-import theTechBlog from "../assets/the-tech-blog.png";
+import fitConnect from "../assets/fit-connect-portfolio.png";
+import shopYourPantry from "../assets/shop-your-pantry-portfolio.png";
+import dragonflyHoroscopes from "../assets/dragonfly-horoscopes-portfolio.png";
+import workDayScheduler from "../assets/work-day-scheduler-portfolio.png";
+import weatherDashboard from "../assets/weather-dashboard-portfolio.png";
+import theTechBlog from "../assets/the-tech-blog-portfolio.png";
 
 function Portfolio() {
   return (
@@ -25,6 +25,8 @@ function Portfolio() {
             title="Fit-Connect"
             deployedLink="https://shrouded-shore-55489.herokuapp.com/"
             githubLink="https://github.com/ayejayhicks/fit-connect"
+            summary="FIT-CONNECT provides a better way for fitness enthusiasts to connect to each other."
+            technologies="MERN stack | Bootstrap | Moment | Yup | Heroku | GitHub"
             img={fitConnect}
           />
         </Col>
@@ -33,6 +35,8 @@ function Portfolio() {
             title="Dragonfly Horoscopes"
             deployedLink="https://dragonfly-horoscopes.herokuapp.com/"
             githubLink="https://github.com/CFox2019/Dragonfly-Horoscopes"
+            summary="Dragonfly Horoscopes is an application that takes in your birthday and provides a daily horoscope"
+            technologies="JavaScript | CSS | Handlebars | Express | MySql | Node | GitHub | Heroku"
             img={dragonflyHoroscopes}
           />
         </Col>
@@ -40,36 +44,52 @@ function Portfolio() {
       <Row>
         <Col size="md-6">
           <Project
-              title="Shop Your Pantry"
-              deployedLink="https://caitie-gorham.github.io/shopYourPantry/"
-              githubLink="https://github.com/CFox2019/shopYourPantry"
-              img={shopYourPantry}
+            title="Shop Your Pantry"
+            deployedLink="https://caitie-gorham.github.io/shopYourPantry/"
+            githubLink="https://github.com/CFox2019/shopYourPantry"
+            summary="Shop Your Pantry allows users to input a list of ingredients they already have
+            in their panty and use those ingredients to generate a list of recipes that incorporate
+            the ingredients. The user can then click on each recipe to find what ingredients they'll
+            need to find and a companion YouTube video for the recipe."
+            technologies="JavaScript | CSS | HTML | API | Bulma | GitHub | GitHub Pages"
+            img={shopYourPantry}
           />
         </Col>
         <Col size="md-6">
           <Project
-              title="Work Day Schedule"
-              deployedLink="https://cfox2019.github.io/Work-Day-Scheduler/"
-              githubLink="https://github.com/CFox2019/Work-Day-Scheduler"
-              img={workDayScheduler}
+            title="Work Day Schedule"
+            deployedLink="https://cfox2019.github.io/Work-Day-Scheduler/"
+            githubLink="https://github.com/CFox2019/Work-Day-Scheduler"
+            summary="With Work Day Schedule you can organize your day at the beginning of each work day.
+            The current date is always posted on the page. The schedule is color coded to help keep your
+            day running smoothly. Past hours are colored gray, current hour is in red, and future hours
+            are in green. Don't forget to save your entries!"
+            technologies="JavaScript | jQuery | CSS | HTML | Bootstrap | Luxon | GitHub | GitHub Pages"
+            img={workDayScheduler}
           />
         </Col>
       </Row>
       <Row>
         <Col size="md-6">
           <Project
-              title="Weather Dashboard"
-              deployedLink="https://cfox2019.github.io/Weather-Dashboard/"
-              githubLink="https://github.com/CFox2019/Weather-Dashboard"
-              img={weatherDashboard}
+            title="Weather Dashboard"
+            deployedLink="https://cfox2019.github.io/Weather-Dashboard/"
+            githubLink="https://github.com/CFox2019/Weather-Dashboard"
+            summary="Plan your day, using Weather Dashboard, by checking the current weather conditions
+            and the 5 Day Forecast for the desired city."
+            technologies="JavaScript | HTML | Bootstrap | Luxon | API | jQuery | GitHub | GitHub Pages"
+            img={weatherDashboard}
           />
         </Col>
         <Col size="md-6">
           <Project
-              title="Tech Blog"
-              deployedLink="https://the-tech-blog.herokuapp.com/"
-              githubLink="https://github.com/CFox2019/The-Tech-Blog"
-              img={theTechBlog}
+            title="Tech Blog"
+            deployedLink="https://the-tech-blog.herokuapp.com/"
+            githubLink="https://github.com/CFox2019/The-Tech-Blog"
+            summary="Tech Blog is a technology based blogging website that allows users to securely create,
+            update, and delete their own blogs, as well as comment on another user's blog."
+            technologies="JavaScript | CSS | Handlebars | Node | Express | MySql | Postman | Heroku | GitHub"
+            img={theTechBlog}
           />
         </Col>
       </Row>
